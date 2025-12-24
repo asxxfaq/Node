921eba3b3becabaf79e970d47797607831f3c4a8
asxxfaq <abdulashfaqehere@gmail.com>
2025-12-24 11:58:17 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+console.log("Hellooo node Js");
+
+
+// Features 
+
+// Asynchronous and non blocking
+// Single Threaded Event loop
+// High Perfomance and cross Platform 
+// NPM 
+// JavaScript Everywhere
+// Rich Ecosystem
+// built in modules
+
+
+
+// Advantages
+
+// Fast Execution
+// Real Time Apps
+// Single Language for front end and backend
+// Large community 
+// Scalability
+// JSON support
